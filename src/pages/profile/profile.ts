21a1ca9f63f83db24955c5c1d2784ec063373c3f
Nicolas Mermoud <nicolas.mermoud@heig-vd.ch>
2018-03-08 10:09:25 +0100

    Added logout functionality to profile page

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { AuthProvider } from '../../providers/auth/auth';
+import { config } from '../../app/config';
+
 /**
  * Generated class for the ProfilePage page.
  *
@@ -14,11 +17,19 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private auth: AuthProvider
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+  }
+
+  logOut() {
+    this.auth.logOut();
   }
 
 }
