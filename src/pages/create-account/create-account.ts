a0ca54e99a5b90d272d08125e3f0717d3138afc9
cecl <cecile.porchet@heig-vd.ch>
2018-03-20 15:50:06 +0100

    sign up form

--- src/pages/create-account/create-account.ts
+++ src/pages/create-account/create-account.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
+
+import { AuthRequest } from '../../models/auth-request';
+import { AuthProvider } from '../../providers/auth/auth';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the CreateAccountPage page.
@@ -13,12 +18,50 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'create-account.html',
 })
 export class CreateAccountPage {
+  /**
+   * This authentication request object will be updated when the user
+   * edits the login form. It will then be sent to the API.
+   */
+  authRequest: AuthRequest;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  /**
+   * If true, it means that the authentication API has return a failed response
+   * (probably because the name or password is incorrect).
+   */
+  loginError: boolean;
+
+  /**
+   * The login form.
+   */
+  @ViewChild(NgForm)
+  form: NgForm;
+
+  constructor(private auth: AuthProvider, private navCtrl: NavController, public navParams: NavParams) {
+        this.authRequest = new AuthRequest();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateAccountPage');
+  }
+
+  onSubmit($event) {
+
+    // Prevent default HTML form behavior.
+    $event.preventDefault();
+
+    // Do not do anything if the form is invalid.
+    if (this.form.invalid) {
+      return;
+    }
+
+    // Hide any previous login error.
+    this.loginError = false;
+
+    // Perform the authentication request to the API.
+    this.auth.logIn(this.authRequest).subscribe(undefined, err => {
+      this.loginError = true;
+      console.warn(`Authentication failed: ${err.message}`);
+    });
   }
 
 }
