3960d467595f3175178853afb9afb0cbd0b3c973
cecl <cecile.porchet@heig-vd.ch>
2018-03-21 15:34:47 +0100

    add form

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { User } from '../../models/user';
+import { NgForm } from '@angular/forms';
+import {  FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { AuthRequest } from '../../models/auth-request';
+import { AuthProvider } from '../../providers/auth/auth';
 
 /**
  * Generated class for the EditProfilePage page.
@@ -16,6 +21,24 @@ import { User } from '../../models/user';
 export class EditProfilePage {
   public user: User;
 
+  /**
+   * This authentication request object will be updated when the user
+   * edits the login form. It will then be sent to the API.
+   */
+  authRequest: AuthRequest;
+
+  /**
+ * If true, it means that the authentication API has return a failed response
+ * (probably because the name or password is incorrect).
+ */
+loginError: boolean;
+
+  /**
+   * The login form.
+   */
+  @ViewChild(NgForm)
+  form: NgForm;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.user = navParams.get('user');
   }
@@ -24,5 +47,21 @@ export class EditProfilePage {
     console.log('ionViewDidLoad EditProfilePage');
     console.log(this.user);
   }
+
+  onSubmit($event) {
+
+  // Prevent default HTML form behavior.
+  $event.preventDefault();
+
+  // Do not do anything if the form is invalid.
+  if (this.form.invalid) {
+    return;
+  }
+
+  // Hide any previous login error.
+  this.loginError = false;
+
+
+}
 
 }
