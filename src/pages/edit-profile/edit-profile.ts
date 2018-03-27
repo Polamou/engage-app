a19016793289aa75dbc043f86021f1161536b14a
Nicolas Mermoud <nicolas.mermoud@heig-vd.ch>
2018-03-27 15:25:02 +0200

    added 'updateUser' function to edit-profile page with phone number genrator for testing

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+
+import { UserProvider } from '../../providers/user/user';
 import { User } from '../../models/user';
 
 /**
@@ -16,13 +18,29 @@ import { User } from '../../models/user';
 export class EditProfilePage {
   public user: User;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public userProvider: UserProvider
+  ) {
     this.user = navParams.get('user');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditProfilePage');
     console.log(this.user);
+  }
+
+  updateUser(){
+    var userPhone = Math.floor((Math.random() * 1000) + 1000);
+    var user = {
+      "phone": userPhone
+    };
+
+    this.userProvider.updateUser(this.user.id, user).subscribe(userResponse => {
+      console.log('user info sent to API');
+      console.log(userResponse);
+    });
   }
 
 }
