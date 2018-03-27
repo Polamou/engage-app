70b4e1b4c2f92fc9a0500e244b0c84ef24e744f0
Nicolas Mermoud <nicolas.mermoud@heig-vd.ch>
2018-03-27 15:08:57 +0200

    add new 'saveNewUser()' function with test users generator

--- src/pages/create-account/create-account.ts
+++ src/pages/create-account/create-account.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { UserProvider } from '../../providers/user/user';
+import { User } from '../../models/user';
+
 /**
  * Generated class for the CreateAccountPage page.
  *
@@ -14,11 +17,34 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CreateAccountPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public userProvider: UserProvider,
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateAccountPage');
+  }
+
+  saveNewUser(){
+    var userName = "testUser"+Math.floor((Math.random() * 100) + 1);
+    var user = {
+      "name": userName,
+      "password": "changeme",
+      "firstname": "Bi",
+      "lastname": "Doum",
+      "phone": "(460) 614-2120",
+      "roles": [
+        "citizen"
+      ]
+    };
+
+    this.userProvider.addUser(user).subscribe(userResponse => {
+      console.log('user info sent to API');
+      console.log(userResponse);
+    });
   }
 
 }
